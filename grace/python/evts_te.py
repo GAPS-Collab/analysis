78c6126edd916aa9daa9d1fc8c9a510230f363d4
Grace Tytus <grace@pevatron.astro.columbia.edu>
2024-12-03 12:59:20 -0500

    TOOL - percent events timed out over time

---
+++ grace/python/evts_te.py
@@ -0,0 +1,63 @@
+import numpy as np
+import sys
+import tqdm
+import pathlib
+from pathlib import Path
+import io
+import contextlib
+import gaps_online as go
+import go_pybindings as gop
+import re
+from glob import glob
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser(prog = 'num events timeout', description = 'produces num events timed out over time as .txt file')
+parser.add_argument('path', help='path to run directory (e.g. /Volumes/gaps-ssd/134/134)')
+parser.add_argument('--writdir')
+parser.add_argument('--id')
+
+args = parser.parse_args()
+
+run_path = Path(args.path)
+files = np.array([str(f) for f in ((run_path.glob('*.tof.gaps')))])
+f_nums = [int(file.split('.')[0].split('_')[-1]) for file in files]
+files = files[np.argsort(f_nums)]
+
+evt_hb = []
+
+analysis_vals = {
+    'te_evt' : [],
+    'met' : [],
+}
+
+for f in tqdm(files, desc="Processing files", unit="file"):
+
+    reader = go.io.TofPacketReader(str(f))
+    for pack in reader:
+        if pack.packet_type == go.io.PacketType.EVTBLDRHeartbeat:
+            hb = go.commands.EVTBLDRHeartbeat()
+            try: 
+                hb.from_tofpacket(pack)
+                nte = hb.n_timed_out
+                nsend = hb.n_sent
+                percent_te = (nte / nsend) * 100
+                analysis_vals['te_evt'].append(percent_te)
+                analysis_vals['met'].append(hb.met_seconds)
+            except Exception as e:
+                print(f"Error: {e}")
+
+with open({args.writdir}.txt, 'w+') as out_file:
+    vals = list(analysis_vals.keys())
+    row = ''    
+    for val in vals:
+        row += val + ','
+    row = row[:-1] + '\n'
+    out_file.write(row)
+    for i in range(len(analysis_vals[vals[0]])):
+        row = ''
+        for val in analysis_vals:
+            row += f'{analysis_vals[val][i]},'
+        row = row[:-1] + '\n'
+        out_file.write(row)
