66f821646d541d239d2f0081c917592bd610e20f
Grace Tytus <gtytus@uhcra01.phys.hawaii.edu>
2025-05-19 15:15:07 -1000

    updates: plots work and produce 3 files. one containing 12pps (umb + cube top and bot), one containing cube sides (8pps) and 1pps, and one containing cortina sides (10pps) and 3pps.

--- grace/python/paddle_occupancy.py
+++ grace/python/paddle_occupancy.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Paddle occupancy graphic plot')
-    parser.add_argument('-dir', --'data_dir', tyep = str, default='', help='A directory with telemetry binaries, as received from the telemetry stream')
+    parser.add_argument('-dir', '--data_dir', type = str, default='', help='A directory with telemetry binaries, as received from the telemetry stream')
     parser.add_argument('-s','--start_time', type=int, default=-1, help='The run start time, e.g. as taken from the elog')
     parser.add_argument('-e','--end_time',type=int, default=-1, help='The run end time, e.g. as taken from the elog')
     parser.add_argument('-id', '--run_id', type=int, help = 'TOF run id' )
@@ -21,9 +21,10 @@ if __name__ == '__main__':
     tofevents = [k.tof for k in data['events']]
     occu = go.tof.analysis.create_occupancy_dict(events = tofevents)
 
-    fig, ax = plt.subplots()
-    fig, ax = go.tof.visual.tof_projection_xy(occu)
-    fig, ax = go.tof.visual.unroll_cbe_sides(paddle_occupancy=occu)
-    fig, ax = go.tof.visual.unroll_cor(paddle_occupancy=occu)
-
-    plt.show()
+    fig1, ax1 = go.tof.visual.tof_projection_xy(occu)
+    fig2, ax2 = go.tof.visual.unroll_cbe_sides(paddle_occupancy=occu)
+    fig3, ax3 = go.tof.visual.unroll_cor(paddle_occupancy=occu)
+    
+    fig1.savefig(str(args.run_id)+'_'+str(args.start_time)+'_'+str(args.end_time)+'_12pps.pdf')
+    fig3.savefig(str(args.run_id)+'_'+str(args.start_time)+'_'+str(args.end_time)+'_10pps_3pps.pdf')
+    fig2.savefig(str(args.run_id)+'_'+str(args.start_time)+'_'+str(args.end_time)+'_8pps_1pps.pdf')
