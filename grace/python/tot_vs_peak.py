a3cd447c3fde6e43c57a398503e06d6d24ed703f
Grace Tytus <gtytus@hawaii.edu>
2025-09-15 10:21:23 -1000

    collapses multiple points per bin into one point per bin

--- grace/python/tot_vs_peak.py
+++ grace/python/tot_vs_peak.py
@@ -1,3 +1,5 @@
+#! /home/gtytus/.rye/shims/python
+
 import gaps_online as go
 from tqdm import tqdm
 import argparse
@@ -14,11 +16,38 @@ def time_over_threshold(wf, threshold=550.0):
     bins_above = np.sum(wf > threshold)
     return bins_above * 0.5  # 0.5 ns per bin
 
+def overlay_mean_rms(xvals, yvals, hinfo, color='red'):
+    """
+    Compute mean and RMS of y per x-bin and overlay errorbars.
+    """
+    H, xedges, yedges, _ = hinfo
+    bin_centers = 0.5 * (xedges[1:] + xedges[:-1])
+
+    means = []
+    rmss  = []
+    xcent = []
+
+    # loop over x-bins
+    for i in range(len(xedges)-1):
+        mask = (xvals >= xedges[i]) & (xvals < xedges[i+1])
+        y_in_bin = np.array(yvals)[mask]
+        if len(y_in_bin) > 0:
+            mean = np.mean(y_in_bin)
+            rms  = np.sqrt(np.mean((y_in_bin - mean)**2))
+            means.append(mean)
+            rmss.append(rms)
+            xcent.append(bin_centers[i])
+
+    plt.errorbar(xcent, means, yerr=rmss, fmt='o', color=color,
+                 markersize=3, elinewidth=1, capsize=2, alpha=0.8)
+
 parser = argparse.ArgumentParser(prog = 'create heatmap of time>550mV vs charge')
 parser.add_argument('-rd', '--raw_dir', default='', help = 'path to .tof.gaps files')
 parser.add_argument('-id', '--run_id', default = 0, help = 'the run id')
 parser.add_argument('-c', required=True, help='path to calibrations dir')
 parser.add_argument('-p', help='path to paddle mapping.csv')
+parser.add_argument('-lb', '--lower_bound', default = 0, type=int, help = 'lower bound of pulse peak height considered')
+parser.add_argument('-ub', '--upper_bound', default = 750, type = int, help = 'upper bound of pulse peak height considered')
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -88,7 +117,7 @@ if __name__ == '__main__':
                             voltages = np.array(waveform.voltages_a)
                             peak  = np.max(voltages)
 
-                            if 600.0 <= peak <= 700.0:
+                            if args.lower_bound <= peak <= args.upper_bound:
                                 time_ns = time_over_threshold(voltages)
                                 peak_all.append(peak)
                                 tot_all.append(time_ns)
@@ -105,7 +134,7 @@ if __name__ == '__main__':
                             voltages = np.array(waveform.voltages_b)
                             peak  = np.max(voltages)
 
-                            if 600.0 <= peak <= 700.0:
+                            if args.lower_bound <= peak <= args.upper_bound:
                                 time_ns = time_over_threshold(voltages)
                                 peak_all.append(peak)
                                 tot_all.append(time_ns)
@@ -119,41 +148,49 @@ if __name__ == '__main__':
     print('Finished reading data')
 
     # --- Plots ---
+    bins_x = np.linspace(0, 20, 40)
+    bins_y = np.linspace(args.lower_bound, args.upper_bound, 200)
     plt.figure()
-    h0 = plt.hist2d(tot_all, peak_all, bins=(200,100), cmap='gnuplot2', norm=colors.LogNorm())
+    h0 = plt.hist2d(tot_all, peak_all, bins=(bins_x, bins_y), cmap='gnuplot2', norm=colors.LogNorm())
     plt.colorbar(h0[3])
+    plt.xlim(0, 20)
+    overlay_mean_rms(tot_all, peak_all, h0, color='black')
     plt.xlabel('Time Over Threshold [nsec]')
     plt.ylabel('Peak Voltage [mV]')
     plt.minorticks_on()
-    plt.savefig('tot_vs_peak_heatmap_all.pdf')
+    plt.savefig(f'tot_vs_peak_{args.lower_bound}_{args.upper_bound}_heatmap_all.pdf')
 
     print('Plot 1/3 done')
 
     plt.figure()
-    h1 = plt.hist2d(tot_a, peak_a,  bins =(200, 100), cmap='gnuplot2', norm=colors.LogNorm())
+    h1 = plt.hist2d(tot_a, peak_a,  bins =(bins_x, bins_y), cmap='gnuplot2', norm=colors.LogNorm())
     plt.colorbar(h1[3])
+    plt.xlim(0, 20)
+    overlay_mean_rms(tot_a, peak_a, h1, color='black')
     plt.xlabel('Time Over Threshold [nsec]')
     plt.ylabel('Peak Voltage [mV]')
     plt.minorticks_on()
-    plt.savefig('tot_vs_peak_heatmap_a_side.pdf')
+    plt.savefig(f'tot_vs_peak_{args.lower_bound}_{args.upper_bound}_heatmap_a_side.pdf')
 
     print('Plot 2/3 done')
 
     plt.figure()
-    h2 = plt.hist2d(tot_b, peak_b, bins = (200,100), cmap='gnuplot2', norm=colors.LogNorm())
+    h2 = plt.hist2d(tot_b, peak_b, bins = (bins_x, bins_y), cmap='gnuplot2', norm=colors.LogNorm())
     plt.colorbar(h2[3])
+    plt.xlim(0,20)
+    overlay_mean_rms(tot_b, peak_b, h2, color='black')
     plt.xlabel('Time Over Threshold [nsec]')
     plt.ylabel('Peak Voltage [mV]')
     plt.minorticks_on()
-    plt.savefig('tot_vs_peak_heatmap_b_side.pdf')
+    plt.savefig(f'tot_vs_peak_{args.lower_bound}_{args.upper_bound}_heatmap_b_side.pdf')
 
     print('Plot 3/3 done')
     print('goodbye and goodluck')
 
-    np.savetxt("tot_all.txt", tot_all)
-    np.savetxt("tot_a.txt", tot_a)
-    np.savetxt("tot_b.txt", tot_b)
+    np.savetxt(f'tot_all_{args.lower_bound}_{args.upper_bound}.txt', tot_all)
+    np.savetxt(f'tot_a_{args.lower_bound}_{args.upper_bound}.txt', tot_a)
+    np.savetxt(f'tot_b_{args.lower_bound}_{args.upper_bound}.txt', tot_b)
 
-    np.savetxt("peak_all.txt", peak_all)
-    np.savetxt("peak_a.txt", peak_a)
-    np.savetxt("peak_b.txt", peak_b)
+    np.savetxt(f'peak_all_{args.lower_bound}_{args.upper_bound}.txt', peak_all)
+    np.savetxt(f'peak_a_{args.lower_bound}_{args.upper_bound}.txt', peak_a)
+    np.savetxt(f'peak_b_{args.lower_bound}_{args.upper_bound}.txt', peak_b)
