7d734b23d9eb23dc7c1bc0850b7e31e16c5c5956
Grace Tytus <gtytus@hawaii.edu>
2026-03-12 14:43:36 -1000

    add max amount of moni values to consider

--- grace/python/sipm_temps/sipm_temp_from_bin.py
+++ grace/python/sipm_temps/sipm_temp_from_bin.py
@@ -2,10 +2,13 @@ import gondola as go
 from glob import glob
 import pandas as pd
 import os
+from tqdm import tqdm
 
+#skip_indices = {2997,2999,3001}
 
+binary_path = '/data/stoessl/flight/GAPSI/flight-starlink-moni-only/'
 #binary_path = '/data1/nextcloud/cra_data/data/binaries_berkeley/starlink/'
-binary_path = '/home/gtytus/data/flight_bin_test/'
+#binary_path = '/home/gtytus/data/flight_bin_test/'
 
 df = pd.read_csv("channel_to_paddle.csv")
 
@@ -17,8 +20,15 @@ channel_to_paddle = {
 paddle_temps = {f"{i}{end}": [] for i in range(1,161) for end in ["A","B"]}
 
 pa_moni_data = go.monitoring.PAMoniDataSeries()
+pa_moni_data.max_size = int(10e6)
 
-for file in sorted(glob(binary_path + '*.bin')):
+files = (sorted(glob(binary_path + '*.bin')))
+
+for idx, file in enumerate(tqdm(files)):
+    #if idx in skip_indices:
+        #print(f"Skipping problematic file {file} at index {idx}")
+        #continue    
+
     basename = os.path.basename(file)
     date_str, time_str = basename[3:9], basename[10:16]
     timestamp = int(date_str + time_str)
