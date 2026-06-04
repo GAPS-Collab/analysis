49f9e6ce9138233aa89f8e856b52a79cedc2aaba
Grace Tytus <gtytus@hawaii.edu>
2026-06-04 05:10:31 -1000

    script to get gps time only from binaries

---
+++ grace/python/sip_gps_from_bin.py
@@ -0,0 +1,35 @@
+import gondola as go
+from glob import glob
+import pandas as pd
+from tqdm import tqdm
+
+#binary_path = '/data/stoessl/flight/GAPSI/flight-starlink-moni-only/'
+binary_path = '/data1/nextcloud/cra_data/data/binaries_berkeley/gcu_2_gcupool/'
+#binary_path = '/data/stoessl/flight/flight-gcu-2-gcupool-moni-only/'
+
+files = (sorted(glob(binary_path + '*.bin')))
+outfile = "sip_gps_time.h5"
+
+first_write = True
+
+for file in tqdm(files):    
+    pos_moni_data = go.monitoring.SipPosMoniDataSeries()
+    pos_moni_data.max_size = int(1e6)
+    
+    pos_moni_data.add_telemetryfile(file)
+    
+    t_0 = pos_moni_data.first_ts
+    df = pos_moni_data.get_dataframe()
+    
+    if first_write == True: 
+        print(df)
+        first_write = False
+        
+    df_out = df.select(["timestamp"]).to_pandas()
+    df_out["timestamp"] = df_out["timestamp"] + t_0
+    
+    df_out.to_hdf(outfile, key='timestamp', mode = 'a', append=not first_write, format='table')
+    
+    del df
+    del df_out
+    del pos_moni_data
